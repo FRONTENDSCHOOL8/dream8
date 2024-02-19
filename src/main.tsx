e03e86a5b434663f713d3e66883fa840e69ab33a
unknown <taewoo1212@gmail>
2024-02-20 01:43:27 +0900

    Vite React 초기 환경구성 taewook 01:43

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../src/app/App';
+import '../src/styles/tailwindcss.css';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ vite.config.ts
@@ -0,0 +1,53 @@
+import path from 'node:path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+import viteImagemin from '@vheemstra/vite-plugin-imagemin';
+import imageminGifSicle from 'imagemin-gifsicle';
+import imageminMozjpeg from 'imagemin-mozjpeg';
+import imageminPngQuant from 'imagemin-pngquant';
+import imageminSvgo from 'imagemin-svgo';
+import imageminWebp from 'imagemin-webp';
+//패키지를 사용하여 이미지를 최적화하면 웹 페이지의 성능을 향상시키고 사용자 경험을 향상
+// https://vitejs.dev/config/
+export default defineConfig({
+  //   base: '/EUID',
+  plugins: [
+    react(),
+    viteImagemin({
+      plugins: {
+        jpg: imageminMozjpeg(),
+        png: imageminPngQuant(),
+        gif: imageminGifSicle(),
+        svg: imageminSvgo(),
+      },
+      makeWebp: {
+        plugins: {
+          jpg: imageminWebp(),
+          png: imageminWebp(),
+        },
+      },
+    }),
+  ],
+  resolve: {
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  css: {
+    devSourcemap: true,
+    modules: {
+      // generateScopedName: '[name]__[local]--[hash:base64:12]',
+    },
+  },
+  build: {
+    cssCodeSplit: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+          reactDom: ['react-dom'],
+          reactRouter: ['react-router-dom'],
+        },
+      },
+    },
+  },
+});
