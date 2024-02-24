c1646d545087d2b9a837cdc92717e384be5febe5
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-24 21:17:34 +0900

    [Chore]: 깃 커밋 컨벤션 생성 - 현돈 21:18
    [Feat] :	새로운 기능 추가
    [Modify] :	코드 변경
    [BugFix] :	버그 수정
    [Comment] :	필요한 주석 추가 및 변경
    [Docs] :	문서의 수정
    [Refactor] :	리팩토링
    [Design] :	CSS 등 사용자 UI 디자인 변경
    [Style] :	코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :	테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :	빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :	프로젝트 초기 생성
    [Rename] :	파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :	파일을 삭제하는 작업만 수행하는 경우
    [Merge] :	Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :	새로운 라이브러리 혹은 패키지

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,7 +7,7 @@ import RootLayout from '@/layout/RootLayout';
 // import NotFound from '@/pages/NotFound';
 
 // 내비게이션(Navigation)
-import navigation from '@/routes/navigation';
+import navigation from '@/routes/navigaition';
 
 // 루트 구성(routes configuration)
 const routes = [
