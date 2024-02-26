cfe91cd9f5dfc594538e2b744da28cbf8f118ae9
lin0211 <slinj0211@gmail.com>
2024-02-26 10:35:10 +0900

    [feat]: 판매 카테고리 구성

--- src/pages/Product/page/Product/Product.tsx
+++ src/pages/Product/page/Product/Product.tsx
@@ -1,5 +1,29 @@
 function Product() {
-  return <div> Product 페이지 </div>;
+  return (
+    <div>
+      <h1 className="sr-only">판매 페이지</h1>
+      <div>
+        <h2 className="sr-only">카테고리</h2>
+        <ul>
+          <li>
+            <button>전체</button>
+            <button>전체</button>
+            <button>전체</button>
+            <button>전체</button>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="sr-only">검색</h2>
+      </div>
+      <div>
+        <h2 className="sr-only">정렬</h2>
+      </div>
+      <div>
+        <h2 className="sr-only">상품 리스트</h2>
+      </div>
+    </div>
+  );
 }
 
 export default Product;
