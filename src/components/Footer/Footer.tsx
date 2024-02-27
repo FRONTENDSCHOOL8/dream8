61911c9e10c0a889ad4fdd5799f83f0b630ec38b
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-27 18:02:35 +0900

    [Design] : Footer 디자인 다시 제작(현돈 : 18:02)
    [Feat] :	새로운 기능 추가
    [Modify] :	코드 변경
    [BugFix] :	버그 수정
    [Comment] :	필요한 주석 추가 및 변경
    [Docs] :	문서의 수정
    [Refactor] :	리팩토링
    [Design] :	CSS 등 사용자 UI 디자인 변경
    [Style] :	코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :	테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :	빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :	프로젝트 초기 생성
    [Rename] :	파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :	파일을 삭제하는 작업만 수행하는 경우
    [Merge] :	Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :	새로운 라이브러리 혹은 패키지

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,33 +1,39 @@
 function Footer() {
   return (
-    <footer className="w-full flex flex-col gap-[23px]  h-[300px] pt-[28px] pb-[24px] px-20">
-      <div className="flex justify-between px-[136]">
-        <div className="flex flex-col ">
+    <footer className="max-w-[90rem] w-full flex flex-col gap-2 fixed bottom-0 bg-gray-300 px-20 text-sm">
+      <div className="flex justify-between pt-1">
+        <div className="flex flex-col gap-3">
           <span className="text-blue-900 font-bold">C/S CENTER</span>
-          <span className="py-[18px] text-xl text-gray-700">123-456-7890</span>
-          <div className="flex flex-col gap-3 text-gray-700">
-            <span>평일 10:100 - 17:00</span>
-            <span>점심시간 12:00 - 13:30</span>
-            <span>주말 및 공휴일 휴무</span>
+          <span className=" text-[1.5rem] text-gray-700">123-456-7890</span>
+          <div className="flex flex-col  text-gray-700">
+            <span className="text-[0.725rem]">평일 10:100 - 17:00</span>
+            <span className="text-[0.725rem]">점심시간 12:00 - 13:30</span>
+            <span className="text-[0.725rem]">주말 및 공휴일 휴무</span>
           </div>
         </div>
 
         <div className="flex flex-col">
           <span className="text-blue-900 font-bold">COMPANY INFO</span>
-          <div className="flex flex-col py-[17px] gap-[10px] text-gray-700">
-            <span>
+          <div className="flex flex-col gap-[0.625rem] text-gray-700">
+            <span className="text-[0.725rem]">
               드림 상점 | 대표 : 정서린 | 사업자등록번호 : 135-15-15302
             </span>
-            <span>주소 : 서울특별시 종로구 사직로 161</span>
-            <span>개인정보 관리 책임자 : 조현돈</span>
+            <span className="text-[0.625rem]">
+              주소 : 서울특별시 종로구 사직로 161
+            </span>
+            <span className="text-[0.625rem]">
+              개인정보 관리 책임자 : 조현돈
+            </span>
           </div>
-          <span className="text-gray-600 text-[13px]">
+          <span className="text-gray-600 text-[0.625rem]">
             이용약관 | 개인정보취급방법
           </span>
         </div>
       </div>
 
-      <div className="text-blue-900 m-auto">© 2024 DREAM STORE</div>
+      <div className="text-blue-900 m-auto text-[0.825rem] pb-2">
+        © 2024 DREAM STORE
+      </div>
     </footer>
   );
 }
