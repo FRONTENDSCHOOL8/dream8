4e42a33d90bfcdbc9384a14ed766eb023d296988
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-27 16:18:07 +0900

    [Feat] : 교환 페이지 디테일 마크업 및 디자인 버튼 구햔(현돈: 16: 19)
    [Feat] :	새로운 기능 추가
    [Modify] :	코드 변경
    [BugFix] :	버그 수정
    [Comment] :	필요한 주석 추가 및 변경
    [Docs] :	문서의 수정
    [Refactor] :	리팩토링
    [Design] :	CSS 등 사용자 UI 디자인 변경
    [Style] :	코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :	테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :	빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :	프로젝트 초기 생성
    [Rename] :	파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :	파일을 삭제하는 작업만 수행하는 경우
    [Merge] :	Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :	새로운 라이브러리 혹은 패키지

--- src/pages/Exchange/page/ExchangeDetail/ExchangeDetails.tsx
+++ src/pages/Exchange/page/ExchangeDetail/ExchangeDetails.tsx
@@ -1,5 +1,49 @@
-function ExchangeDetails() {
-  return <div> 교환 상세 페이지</div>;
+import { useParams } from 'react-router-dom';
+import { useExchangeStore } from '@/pages/Exchange/page/Exchange/Exchange';
+import ExchangeBox from '../../atom/ExchangeBox';
+import { getPbImageURL } from '@/utils/getPbImage';
+import Profile from '@/components/Profile/Profile';
+import ExchangeModify from '@/pages/Exchange/molecules/ExchangeModify';
+import { ProductDetails } from '@/pages';
+
+function ExchangeDetailPage() {
+  const { id } = useParams();
+  const { exchangeData } = useExchangeStore();
+
+  const selectedItem = exchangeData.find((item) => item.id === id);
+  console.log(selectedItem);
+
+  if (!selectedItem) {
+    return <div>해당 아이템을 찾을 수 없습니다.</div>;
+  }
+
+  return (
+    <>
+      <div className="mt-10 flex w-full">
+        <img
+          src={getPbImageURL(selectedItem, 'product_img')}
+          className="w-[17.5rem] border rounded-xl"
+        />
+        <div className="flex flex-col pl-14 gap-6">
+          <p>{selectedItem.type}</p>
+          <h2 className="text-2xl">{selectedItem.title}</h2>
+          <ExchangeBox name="교환가능"></ExchangeBox>
+          <div className="border border-gray-200 p-2">
+            <Profile />
+          </div>
+          <p className="text-gray-100 text-xs">
+            중고 물품 물물 거래만 가능합니다. <br />
+            금전 거래를 자제해주세요.
+          </p>
+          <ExchangeModify />
+        </div>
+      </div>
+      <div className="pt-14">
+        <h1 className="text0">상품정보</h1>
+        <p className="w-11/12 border h-52 p-4">{selectedItem.product_detail}</p>
+      </div>
+    </>
+  );
 }
 
-export default ExchangeDetails;
+export default ExchangeDetailPage;
