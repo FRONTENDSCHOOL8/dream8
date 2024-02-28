2a8ded448c37faa8e2cae1901dade7da02daf37a
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-28 10:28:28 +0900

    [Design] : 푸터 이용약관 색상변경(현돈 : 10:28)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -14,7 +14,7 @@ function Footer() {
 
         <div className="flex flex-col">
           <span className="text-blue-900 font-bold">COMPANY INFO</span>
-          <div className="flex flex-col gap-[0.625rem] text-gray-700">
+          <div className="flex flex-col gap-[0.625rem]">
             <span className="text-[0.725rem]">
               드림 상점 | 대표 : 정서린 | 사업자등록번호 : 135-15-15302
             </span>
@@ -25,7 +25,7 @@ function Footer() {
               개인정보 관리 책임자 : 조현돈
             </span>
           </div>
-          <span className="text-gray-600 text-[0.625rem]">
+          <span className="text-gray-500 text-[0.625rem]">
             이용약관 | 개인정보취급방법
           </span>
         </div>
