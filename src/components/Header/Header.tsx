4116aad75531518a859a950958d32559e8356f78
unknown <taewoo1212@gmail>
2024-02-28 23:39:33 +0900

    [Feat] : 회원가입, 로그인여부로 로그인,회원가입 표시 taewook 23:39

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import { Link } from 'react-router-dom';
 import GlobalNavBar from './GlobalNavBar';
+import { useLoginFormStore } from '@/store/useLoginFormStore';
 
 function Header() {
+  const { isLoggedIn } = useLoginFormStore();
+  console.log('isLoggedIn   ', isLoggedIn);
   return (
     <header className="fixed w-full bg-white shadow-root z-50">
       <div className="max-w-[1400px] m-auto">
         <div className="flex items-start justify-end text-sm gap-2">
-          <Link to="/SignUp">회원가입</Link>
-          <Link to="/SignIn">로그인</Link>
+          {isLoggedIn && <Link to="/SignUp">회원가입</Link>}
+          {!isLoggedIn && <Link to="/SignIn">로그인</Link>}
         </div>
         <GlobalNavBar />
       </div>
