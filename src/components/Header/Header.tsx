7364afa55a84604938d9ae32bbf60f4d1dea3664
unknown <taewoo1212@gmail>
2024-02-29 08:58:51 +0900

    [Feat] : 회원가입 항상표시로 수정 taewook 08:59

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ function Header() {
     <header className="fixed w-full bg-white shadow-root z-50">
       <div className="max-w-[1400px] m-auto">
         <div className="flex items-start justify-end text-sm gap-2">
-          {isLoggedIn && <Link to="/SignUp">회원가입</Link>}
+          <Link to="/SignUp">회원가입</Link>
           {!isLoggedIn && <Link to="/SignIn">로그인</Link>}
         </div>
         <GlobalNavBar />
