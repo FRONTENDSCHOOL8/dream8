dcb8ba8086ef4ac2fe09fc8b31a5caa989993c45
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-28 18:18:06 +0900

    [Refactor] : URL을 API로 수정(현돈: 18:18)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/utils/getPbImage.ts
+++ src/utils/getPbImage.ts
@@ -1,5 +1,5 @@
 export function getPbImageURL(item, fileName = 'photo'): string {
-  const pbUrl = import.meta.env.VITE_PB_URL;
+  const pbUrl = import.meta.env.VITE_PB_API;
 
   return `${pbUrl}/api/files/${item.collectionId}/${item.id}/${item[fileName]}`;
 }
