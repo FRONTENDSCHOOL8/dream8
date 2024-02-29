6a2991a1643d89cc148ae98b18bc9120beb22d89
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-29 14:17:57 +0900

    [Refactor] : 더보기 버튼 바깥으로 수정(현돈: 14:18)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/pages/Exchange/molecules/ExchangeMore.tsx
+++ src/pages/Exchange/molecules/ExchangeMore.tsx
@@ -1,11 +1,9 @@
-import MoreButton from '@/components/Button/MoreButton';
 import ExchangeLogin from '../atom/ExchangeLogin';
 import ExchangeLoginText from '../atom/ExchangeLoginText';
 
 function ExchangeMore() {
   return (
     <div className="pt-2 flex flex-col justify-center items-center gap-3">
-      <MoreButton />
       <ExchangeLoginText />
       <ExchangeLogin />
     </div>
