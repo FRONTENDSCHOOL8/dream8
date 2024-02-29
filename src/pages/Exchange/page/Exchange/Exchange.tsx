cc5c92e9438f577a23ff0c04d74e55f72defe783
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-02-29 14:18:29 +0900

    [Refactor] : 로그인 되면 리스트 보이도록 렌더링 (현돈: 14:18)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/pages/Exchange/page/Exchange/Exchange.tsx
+++ src/pages/Exchange/page/Exchange/Exchange.tsx
@@ -1,14 +1,13 @@
-import ExchangeWriteButton from '@/components/Button/ExchangeWriteButton';
-import Card from '@/components/Card/Card';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import create from 'zustand';
 import { pb } from '@/api/pocketbase';
 import ExchangeMore from '@/pages/Exchange/molecules/ExchangeMore';
+import { useLoginFormStore } from '@/store/useLoginFormStore';
+import MoreButton from '@/components/Button/MoreButton';
+import ExchangeWriteButton from '@/components/Button/ExchangeWriteButton';
+import Card from '@/components/Card/Card';
 
-const user = {
-  name: 'donny',
-};
 interface ExchangeItem {
   id: string;
   title: string;
@@ -33,53 +32,52 @@ export const useExchangeStore = create<ExchangeStore>((set) => ({
 
 function Exchange() {
   const { exchangeData, setExchangeData } = useExchangeStore();
-  const maxItemsToShow = 6;
+  const { isLoggedIn } = useLoginFormStore();
+  const [maxList, setMaxList] = useState(6);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const resultList: ExchangeItem[] = await pb
-          .collection('exchange')
-          .getFullList();
-        setExchangeData(resultList);
-      } catch (error) {
-        console.error('Error fetching exchange data:', error);
-      }
-    };
-
     fetchData();
-  }, [setExchangeData]);
+  }, []);
 
-  if (!user || exchangeData.length === 0) {
-    return null;
-  }
+  const fetchData = async () => {
+    try {
+      const resultList: ExchangeItem[] = await pb
+        .collection('exchange')
+        .getFullList();
+      setExchangeData(resultList);
+    } catch (error) {
+      console.error('Error fetching exchange data:', error);
+    }
+  };
 
-  const visibleExchangeData = exchangeData.slice(0, maxItemsToShow);
+  const handleMoreButtonClick = () => {
+    setMaxList((prevMaxList) => prevMaxList + 6);
+  };
 
   return (
     <div className="flex flex-col gap-4 pt-10">
       <div className="flex flex-col justify-center items-center gap-2">
-        <div className="w-6/12 flex justify-end">
+        <div className="w-[56rem] flex justify-end">
           <ExchangeWriteButton name="게시글 작성" />
         </div>
-        <div
-          className={`${
-            maxItemsToShow
-              ? 'grid grid-cols-3 gap-10 w-7/12'
-              : 'grid grid-cols-3 grid-rows-3 gap-10 w-7/12'
-          }`}
-        >
-          {visibleExchangeData.map((item, index) => (
-            <Link
-              to={`/Exchange/ExchangeDetail/${item.id}`}
-              key={item.id}
-              className={`${index >= 3 ? 'opacity-30' : ''}`}
-            >
-              <Card>{item}</Card>
-            </Link>
-          ))}
+        <div className="grid grid-cols-3 gap-10 w-[60rem]">
+          {exchangeData.slice(0, maxList).map((item, index) =>
+            isLoggedIn ? (
+              <Link to={`/Exchange/ExchangeDetail/${item.id}`} key={item.id}>
+                <Card>{item}</Card>
+              </Link>
+            ) : (
+              <Link
+                to="/SignIn"
+                className={`${!isLoggedIn && index >= 3 ? 'opacity-30' : ''}`}
+              >
+                <Card>{item}</Card>
+              </Link>
+            )
+          )}
         </div>
-        <ExchangeMore />
+        <MoreButton onClick={handleMoreButtonClick} />
+        {!isLoggedIn && <ExchangeMore />}
       </div>
     </div>
   );
