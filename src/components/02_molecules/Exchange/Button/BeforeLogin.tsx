20e1ce47000c817385934fa94d84a9beb29c0aa0
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-03-03 15:07:24 +0900

    [Refactor] : 02_molecules로 교환메인페이지 더보기 파일 분리(현돈 : 15:08)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/02_molecules/Exchange/Button/BeforeLogin.tsx
+++ src/components/02_molecules/Exchange/Button/BeforeLogin.tsx
@@ -1,8 +1,15 @@
+import Button from '@/components/01_atoms/Button/Button';
 import { Link } from 'react-router-dom';
 
 function BeforeLogin() {
   return (
-    <>
+    <div className="flex flex-col items-center gap-2">
+      <Button
+        type="button"
+        className="rounded-md px-1 text-blue-primary border-blue-primary border hover:bg-blue-primary hover:text-white"
+      >
+        더보기
+      </Button>
       <p>로그인 하시고 더 많은 중고거래를 확인하세요</p>
       <Link
         to="/SignIn"
@@ -10,7 +17,7 @@ function BeforeLogin() {
       >
         로그인하러가기
       </Link>
-    </>
+    </div>
   );
 }
 
