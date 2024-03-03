3914f94a3c87b3be25eab74f1c832a5b2e3acd9f
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-03-03 15:08:42 +0900

    [Refactor] : 교환 메인 페이지 카드 02_molecules에 분리 (현돈: 15:09)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/02_molecules/Exchange/ExchangeCard/ExchangeCard.tsx
+++ src/components/02_molecules/Exchange/ExchangeCard/ExchangeCard.tsx
@@ -1,35 +1,34 @@
-import Button from '@/components/01_atoms/Button/Button';
 import User from '@/components/02_molecules/Exchange/User/User';
+import { getPbImageURL } from '@/utils/getPbImage';
 
-interface ExchangeCard {
-  imageSrc: string;
-  className?: string;
+interface ExchangeChildren {
   title: string;
-  content: string;
-  name: string;
-  imageAlt: string;
+  product_detail: string;
 }
 
-function ExchangeCard({
-  imageSrc,
-  className,
-  title,
-  content,
-  name,
-  imageAlt,
-}: ExchangeCard) {
+interface ExchangeCard {
+  name: string;
+  children: ExchangeChildren;
+}
+
+function ExchangeCard({ name, children }: ExchangeCard) {
+  const { title, product_detail } = children;
+
   return (
-    <div className={className}>
-      <img src={imageSrc} alt={imageAlt} />
-      <User />
-      <h2>{title}</h2>
-      <p>{content}</p>
-      <Button
-        type="button"
-        className="bg-orange-primary text-white items-center text-[0.85rem] px-1 rounded-md"
-      >
-        {name}
-      </Button>
+    <div className="rounded-2xl shadow-root text-ellipsis flex flex-col gap-2">
+      <img
+        src={getPbImageURL(children, 'product_img')}
+        className="rounded-t-2xl h-44 line-clamp-1 w-full"
+        alt={product_detail}
+      />
+      <div className="flex flex-col gap-3 p-2">
+        <User />
+        <h2 className="text-[1.1rem] line-clamp-1">{title}</h2>
+        <p className="text-[0.725rem] line-clamp-1">{product_detail}</p>
+        <div className="bg-orange-primary text-white w-14 text-[0.875rem] flex justify-center items-center rounded-[0.3125rem]">
+          {name}
+        </div>
+      </div>
     </div>
   );
 }
