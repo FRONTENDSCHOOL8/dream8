253ae2c43c41b7dfe347493e87bee2f3b6532e80
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-03-06 13:26:45 +0900

    [Feat] : 사용자 이름 나오도록 구현(현돈: 13:27)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/05_pages/Exchange/ExchangeDetail.tsx
+++ src/components/05_pages/Exchange/ExchangeDetail.tsx
@@ -1,24 +1,24 @@
 import { useParams } from 'react-router-dom';
 import { getPbImageURL } from '@/utils/getPbImage';
-import Profile from '@/components/Profile/Profile';
 import ExchangeModify from '@/pages/Exchange/molecules/ExchangeModify';
-import Exchangeable from '@/components/01_atoms/StateBox/StateBox';
 import { useListStore } from '@/store/useListStore';
+import StateBox from '@/components/StateBox/StateBox';
+import User from '@/components/02_molecules/Exchange/User/User';
 
 function ExchangeDetail() {
   const { id } = useParams();
   const { Data } = useListStore();
 
   const selectedItem = Data.find((item) => item.id === id);
-  console.log(selectedItem);
+  const userName = selectedItem?.expand.field[0].user_name;
 
   if (!selectedItem) {
     return <div>해당 아이템을 찾을 수 없습니다.</div>;
   }
 
   return (
-    <div className="pl-20" style={{ height: `calc(100vh - 82px - 156px)` }}>
-      <div className="pt-10 flex">
+    <div className="p-10">
+      <div className=" flex">
         <img
           src={getPbImageURL(selectedItem, 'product_img')}
           className="w-[18.75rem] h-[18.75rem] rounded-xl"
@@ -26,12 +26,12 @@ function ExchangeDetail() {
         <div className="flex flex-col pl-14 gap-6 ">
           <p>{selectedItem.type}</p>
           <h2 className="text-2xl">{selectedItem.title}</h2>
-          <Exchangeable
+          <StateBox
             className="bg-orange-primary text-white w-14 text-[0.875rem] flex justify-center items-center rounded-[0.3125rem]"
             name="교환가능"
-          ></Exchangeable>
+          ></StateBox>
           <div className="border border-gray-200 p-2">
-            <Profile />
+            <User userName={userName} />
           </div>
           <p className="text-gray-100 text-xs">
             중고 물품 물물 거래만 가능합니다. <br />
