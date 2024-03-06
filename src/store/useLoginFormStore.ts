412b4dec0af9cedcbd324deaa72e937ef004919c
unknown <taewoo1212@gmail>
2024-03-06 10:07:33 +0900

    [Merge] :  useLoginFormStore Accept Incomming 충돌 해결 태욱 10:08

--- src/store/useLoginFormStore.ts
+++ src/store/useLoginFormStore.ts
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-// import { create } from 'zustand';
-
-// interface LoginFormState {
-//   userInfo: any;
-//   email: string;
-//   password: string;
-//   isLoggedIn: boolean;
-//   setIsLoggedIn: (isLoggedIn: boolean) => void;
-//   setEmail: (email: string) => void;
-//   setPassword: (password: string) => void;
-//   setUserInfo: (userInfo: any) => void;
-// }
-
-// const useLoginFormStore = create<LoginFormState>((set) => {
-//   const storedUserInfo = localStorage.getItem('userInfo');
-//   const storedIsLoggedIn = localStorage.getItem('isLoggedIn');
-
-//   return {
-//     email: '',
-//     password: '',
-//     isLoggedIn: storedIsLoggedIn ? JSON.parse(storedIsLoggedIn) : false,
-//     setIsLoggedIn: (isLoggedIn) => {
-//       set({ isLoggedIn });
-//       localStorage.setItem('isLoggedIn', JSON.stringify(isLoggedIn));
-//     },
-//     userInfo: storedUserInfo ? JSON.parse(storedUserInfo) : '',
-//     setUserInfo: (userInfo) => {
-//       set({ userInfo });
-//       localStorage.setItem('userInfo', JSON.stringify(userInfo));
-//     },
-//     setEmail: (email: string) => set({ email }),
-//     setPassword: (password: string) => set({ password }),
-//   };
-// });
-=======
 import { create } from 'zustand';
 import { devtools, persist } from 'zustand/middleware';
 
@@ -50,6 +14,5 @@ const useLoginFormStore = create(
     { name: 'userStore' }
   )
 );
->>>>>>> upstream/develop
 
-// export default useLoginFormStore;
+export default useLoginFormStore;
