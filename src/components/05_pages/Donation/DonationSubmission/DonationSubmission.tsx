a0c68325ceea1f959ed78153a46e65d7c9f5dc7e
yeoseoyun <yeosummer94@gmail.com>
2024-03-06 13:14:46 +0900

    [Design] :  후원 신청 폼 css 수정

--- src/components/05_pages/Donation/DonationSubmission/DonationSubmission.tsx
+++ src/components/05_pages/Donation/DonationSubmission/DonationSubmission.tsx
@@ -30,7 +30,7 @@ function DonationSubmission() {
 
   return (
     <div className="py-20">
-      <div className="flex flex-col gap-8 items-center justify-center w-[1024px] h-[1119px] m-auto border border-gray-200 rounded-[50px]">
+      <div className="flex flex-col gap-8 items-center justify-center w-[1024px] m-auto py-20 border border-gray-200 rounded-[50px]">
         <h2 className="text-4xl">후원 신청</h2>
 
         <div className='max-w-[595px] flex flex-col gap-10 items-center'>
