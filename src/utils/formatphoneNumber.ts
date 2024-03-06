ca5d800eca4ef6be93ef0ec5491edf7e750e2569
unknown <taewoo1212@gmail>
2024-03-06 19:43:03 +0900

    [Feat] : 2 formatPhoneNumber 수정 , 거래내역, 후원내역 태욱 18:30

--- src/utils/formatphoneNumber.ts
+++ src/utils/formatphoneNumber.ts
@@ -1,5 +1,6 @@
-const formatPhoneNumber = (phoneNumber: string) => {
-  // 숫자만 추출하여 000-0000-0000 형식으로 변환!
+// 폰 번호를 원하는 형식으로 변경하는 함수
+export const formatPhoneNumber = (phoneNumber: string) => {
+  // 숫자만 추출하여 000-0000-0000 형식으로 변환
   const cleaned = phoneNumber.replace(/\D/g, '');
   const match = cleaned.match(/^(\d{3})(\d{4})(\d{4})$/);
 
@@ -9,5 +10,3 @@ const formatPhoneNumber = (phoneNumber: string) => {
 
   return phoneNumber;
 };
-
-export default formatPhoneNumber;
