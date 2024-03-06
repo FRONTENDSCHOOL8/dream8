a1ad08adcb914ffa644120d413eb8c13ea46a499
unknown <taewoo1212@gmail>
2024-03-06 18:18:14 +0900

    [Feat] : formatPhoneNumber 수정 태욱 18:20

--- src/utils/formatphoneNumber.ts
+++ src/utils/formatphoneNumber.ts
@@ -1,5 +1,4 @@
-// 폰 번호를 원하는 형식으로 변경하는 함수
-export const formatPhoneNumber = (phoneNumber: string) => {
+const formatPhoneNumber = (phoneNumber: string) => {
   // 숫자만 추출하여 000-0000-0000 형식으로 변환
   const cleaned = phoneNumber.replace(/\D/g, '');
   const match = cleaned.match(/^(\d{3})(\d{4})(\d{4})$/);
@@ -8,5 +7,7 @@ export const formatPhoneNumber = (phoneNumber: string) => {
     return `${match[1]}-${match[2]}-${match[3]}`;
   }
 
-  return formatPhoneNumber;
+  return phoneNumber;
 };
+
+export default formatPhoneNumber;
