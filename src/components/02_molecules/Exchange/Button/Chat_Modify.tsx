983b670610a57e25733ee75711c9d8bcf60b635e
cho hyun don <113508075+chohyundon@users.noreply.github.com>
2024-03-09 20:55:50 +0900

    [Feat] : 데이터삭제 되면 모달창 뜨도록 구현 + 교환 메인 페이지로 이동(현돈: 20:56)
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/02_molecules/Exchange/Button/Chat_Modify.tsx
+++ src/components/02_molecules/Exchange/Button/Chat_Modify.tsx
@@ -1,6 +1,10 @@
 import Button01 from '@/components/01_atoms/Button/Button01';
 import Trash from '/Trash.svg';
 import { useNavigate } from 'react-router-dom';
+import useDelete from '@/hooks/useDelete';
+import { useEffect, useState } from 'react';
+import SelectModal from '@/components/02_molecules/Modal/SelectModal/SelectModal';
+import ConfirmModal from '../../Modal/ConfirmModal/ConfirmModal';
 
 interface EditProp {
   Edit: boolean;
@@ -8,7 +12,36 @@ interface EditProp {
 }
 
 function ExchangeModify({ Edit, id }: EditProp) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isSuccessModalOpen, setIsSuccessModalOpen] = useState(false);
+
+  const text = {
+    title: '경고',
+    message: '진짜로 삭제하실건가요?',
+  };
+
   const navigate = useNavigate();
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    if (isSuccessModalOpen) {
+      const timeoutId = setTimeout(() => {
+        setIsSuccessModalOpen(false);
+        setIsOpen(false);
+        navigate('/Exchange');
+      }, 2000);
+
+      return () => clearTimeout(timeoutId);
+    }
+  }, [isSuccessModalOpen]);
+
+  const handleDelete = () => {
+    useDelete(id);
+    setIsSuccessModalOpen(true);
+  };
 
   return Edit ? (
     <div className="flex items-center gap-4">
@@ -19,9 +52,25 @@ function ExchangeModify({ Edit, id }: EditProp) {
       >
         게시글 수정하기
       </Button01>
-      <Button01 className="p-0 border-none" type="button">
-        <img src={Trash} />
+      <Button01
+        className="p-0 border-none"
+        type="button"
+        onClick={() => setIsOpen(true)}
+      >
+        <img src={Trash} alt="삭제버튼" />
       </Button01>
+      {isOpen && (
+        <SelectModal
+          title={text.title}
+          onClickNo={handleCloseModal}
+          onClickYes={handleDelete}
+        >
+          {text.message}
+        </SelectModal>
+      )}
+      {isSuccessModalOpen && (
+        <ConfirmModal title="성공">데이터 삭제를 성공했습니다</ConfirmModal>
+      )}
     </div>
   ) : (
     <Button01
@@ -29,7 +78,7 @@ function ExchangeModify({ Edit, id }: EditProp) {
       className="p-0 w-80 h-10 hover:bg-blue-primary hover:text-white"
     >
       채팅하기
-    </Button01> // Edit이 false일 때 아무것도 렌더링하지 않음
+    </Button01>
   );
 }
 
