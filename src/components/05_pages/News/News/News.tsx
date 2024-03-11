2d88b64c6bdda39fa375b80c70ab84d4175ca6d4
yeoseoyun <yeosummer94@gmail.com>
2024-03-11 23:14:16 +0900

    [Feat] :  News 페이지 NewsList 더보기 버튼 구현

--- src/components/05_pages/News/News/News.tsx
+++ src/components/05_pages/News/News/News.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useQuery } from '@tanstack/react-query';
 import Title from "../../../01_atoms/News/NewsTitle/NewsTitle";
 import NewsList from "../../../01_atoms/News/NewsList/NewsList";
+import Button from '@/components/01_atoms/Button/Button';
 import { pb } from "@/api/pocketbase";
 import { useLoaderData } from "react-router";
 
@@ -25,6 +26,14 @@ export function News() {
     }
   }, [newsList]);
 
+  const [ showMore, setShowMore ] = useState(5);
+
+  const handleShowMore = () => {
+    if (showMore < newsList.length) {
+      setShowMore(showMore + 3);
+    }
+  };
+
   return (
     <div className="max-w-[90rem] m-auto bg-white py-20">
       <Title />
@@ -32,7 +41,18 @@ export function News() {
         <div className="py-10">
           <p className="text-end">총 <strong className="text-blue-primary">{newsCount}</strong>개의 소식이 있습니다.</p>
         </div>
-        <NewsList newsList={newsList} />
+        <NewsList newsList={newsList.slice(0, showMore)} />
+        {showMore < newsList.length && (
+          <div className="flex justify-center">
+            <Button
+              type="button"
+              onClick={handleShowMore}
+              className="border p-2 rounded-xl text-gray-500 m-auto text-lg hover:text-white hover:bg-blue-primary"
+            >
+              더보기
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
