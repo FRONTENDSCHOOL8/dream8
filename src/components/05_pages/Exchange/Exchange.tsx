f4ed0d199a6391594c69c32cbcfa04b98249c5b6
yeoseoyun <yeosummer94@gmail.com>
2024-03-13 14:56:12 +0900

    [Design] :  교환css수정 (서윤 14:56)

--- src/components/05_pages/Exchange/Exchange.tsx
+++ src/components/05_pages/Exchange/Exchange.tsx
@@ -71,7 +71,7 @@ export function Exchange() {
   return (
     <div className="flex flex-col gap-4 pt-32 items-center max-w-[90rem] m-auto">
       <MetaTag metaTag={metaTagData} />
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-10">
         <div className="flex justify-end">
           <Link to={isLoggedIn ? '/ExchangeWrite' : '/SignIn'}>
             <Button
@@ -89,7 +89,7 @@ export function Exchange() {
           {isLoggedIn ? (
             <Button01
               type="button"
-              className="rounded-md p-0 w-[4rem] border-2 text-sm hover:bg-blue-primary hover:text-white"
+              className="border p-2 rounded-xl text-gray-500 m-auto text-lg hover:text-white hover:bg-blue-primary"
               onClick={handleLoadMoreButtonClick}
             >
               더보기
@@ -98,7 +98,7 @@ export function Exchange() {
             <div className="flex flex-col items-center">
               <Button01
                 type="button"
-                className="rounded-md p-0 w-[4rem] border-2 text-sm hover:bg-blue-primary hover:text-white"
+                className="border p-2 rounded-xl text-gray-500 m-auto text-lg hover:text-white hover:bg-blue-primary"
                 onClick={handleClick}
               >
                 더보기
