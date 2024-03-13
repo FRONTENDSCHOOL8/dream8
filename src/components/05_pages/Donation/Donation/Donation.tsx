7ecc62973a5f50a6563d5040ceaf3a0cec7ee49d
yeoseoyun <yeosummer94@gmail.com>
2024-03-13 09:06:15 +0900

    [Style] :  문구수정

--- src/components/05_pages/Donation/Donation/Donation.tsx
+++ src/components/05_pages/Donation/Donation/Donation.tsx
@@ -4,7 +4,7 @@ import MetaTag from '@/components/01_atoms/MetaTag/MetaTag';
 function Donation() {
   const metaTag = {
     title: '후원페이지',
-    pageDescription: '드림의 후원 페이지 입니다',
+    pageDescription: '드림의 후원페이지 입니다',
     keywords: 'dream, 판매, 헌옷, 기부, 후원, 지구사랑, 환경, 공헌',
     imgSrc: '/logoOG.png',
     path: 'Donation',
