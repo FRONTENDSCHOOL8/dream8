a96ec6c1fcc94f50b5834f2723a1a60eb520ff6f
unknown <taewoo1212@gmail>
2024-03-24 20:02:35 +0900

    [Feat] : 로그인, 회원가입 validation 추가 [react-hook-form 라이브러리 사용] 태욱

--- src/components/02_molecules/SignUp/SignUpInput.tsx
+++ src/components/02_molecules/SignUp/SignUpInput.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { pb } from '@/api/pocketbase';
-import Input from '@/components/01_atoms/Input/Input';
+import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 
 interface UserData {
   user_name: string;
@@ -14,79 +13,44 @@ interface UserData {
 }
 
 function SignUpInput() {
-  const [userData, setUserData] = useState<UserData>({
-    user_name: '',
-    nickName: '',
-    password: '',
-    passwordConfirm: '',
-    email: '',
-    phone_number: '',
-    address: '',
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm({
+    mode: 'onChange',
   });
 
   const navigate = useNavigate();
 
-  const handleAddress = (e) => {
+  const handleAddress = () => {
     new daum.Postcode({
-      oncomplete: function (data) {
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          address: data.jibunAddress,
-        }));
+      oncomplete: function (data: { jibunAddress: string | number }) {
+        setValue('address', data.jibunAddress);
         // 주소 선택 후 입력 창에 주소 채우기
-
-        setUserData((prevUserData) => ({
-          ...prevUserData,
-          address: data.jibunAddress,
-        }));
       },
     }).open();
   };
-  console.log('userData  ', userData);
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
 
-    let isValid = true;
-
-    // 각 input 요소의 값을 확인하고 유효성 검사
-    if (
-      !userData.email ||
-      !userData.password ||
-      !userData.passwordConfirm ||
-      !userData.user_name ||
-      !userData.nickName ||
-      !userData.phone_number ||
-      !userData.address
-    ) {
-      alert('모든 필드를 입력해주세요.');
-      isValid = false;
-    }
-
-    if (!isValid) return;
-
-    // 비밀번호와 비밀번호 확인 값이 일치하는지 확인
-    if (userData.password !== userData.passwordConfirm) {
-      alert('비밀번호와 비밀번호 확인 값이 일치하지 않습니다.');
-      return;
-    }
-
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
     // 회원가입 처리 로직 추가
-
     try {
       // 데이터베이스에서 이미 가입된 회원 정보 가져오기
       const existingUsers = await pb.collection('users').getFullList();
 
       // 각 입력값과 데이터베이스에 있는 회원 정보 비교하여 중복 확인
       for (const user of Array.from(existingUsers)) {
-        for (const key of Object.keys(userData)) {
-          if (userData[key as keyof UserData] === user[key as keyof UserData]) {
+        for (const key of Object.keys(data)) {
+          if (data[key as keyof UserData] === user[key as keyof UserData]) {
             alert(`${key}이(가) 이미 사용 중입니다. 다른 값을 입력해주세요.`);
             return;
           }
         }
       }
 
-      await pb.collection('users').create(userData);
+      await pb.collection('users').create(data);
       alert('회원가입이 완료되었습니다.');
     } catch (error) {
       console.error('Error logging in:', error);
@@ -96,113 +60,182 @@ function SignUpInput() {
   };
 
   return (
-    <form className="w-[30rem] flex flex-col gap-7" onSubmit={handleSubmit}>
+    <form
+      className="w-[30rem] flex flex-col gap-7"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div>
         <label htmlFor="email">이메일</label>
-        <Input
-          type="email"
-          id="email"
-          required
-          aria-required="true"
-          placeholder="이메일을 입력해주세요"
-          onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+        <input
+          {...register('email', {
+            required: '이메일을 필수 입력입니다.',
+            pattern: {
+              value:
+                /^[a-zA-Z]+[!#$%&'*+-/=?^_`(){|}~]*[a-zA-Z0-9]*@[\w]+\.[a-zA-Z0-9-]+[.]*[a-zA-Z0-9]+$/,
+              message: '이메일 형식이 아닙니다.',
+            },
+          })}
+          aria-invalid={errors.email ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.email?.message === 'string'
+            ? errors.email.message
+            : ''}
+        </p>
       </div>
       <div>
         <label htmlFor="password">비밀번호</label>
-        <Input
-          type="password"
+        <input
           id="password"
-          required
-          aria-required="true"
-          placeholder="비밀번호를 입력해주세요"
-          onChange={(e) =>
-            setUserData({ ...userData, password: e.target.value })
-          }
+          type="password"
+          placeholder="*******"
+          {...register('password', {
+            required: '비밀번호는 필수 입력입니다.',
+            pattern: {
+              value:
+                /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+              message:
+                '비밀번호는 8자 이상이어야 하며, 숫자/대문자/소문자/특수문자를 모두 포함해야 합니다.',
+            },
+          })}
+          aria-invalid={errors.password ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.password?.message === 'string'
+            ? errors.password.message
+            : ''}
+        </p>
       </div>
       <div>
         <label htmlFor="passwordConfirm">비밀번호 확인</label>
-        <Input
-          type="password"
+        <input
           id="passwordConfirm"
-          required
-          aria-required="true"
-          placeholder="비밀번호를 한번더 입력해주세요"
-          onChange={(e) =>
-            setUserData({ ...userData, passwordConfirm: e.target.value })
-          }
+          type="password"
+          placeholder="*******"
+          {...register('passwordConfirm', {
+            required: '비밀번호 확인은 필수 입력입니다',
+            validate: {
+              check: (passwordConfirmValue) => {
+                if (getValues('password') !== passwordConfirmValue) {
+                  return '비밀번호가 일치하지 않습니다.';
+                }
+              },
+            },
+          })}
+          aria-invalid={errors.passwordConfirmValue ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.passwordConfirm?.message === 'string'
+            ? errors.passwordConfirm.message
+            : ''}
+        </p>
       </div>
+
       <div>
         <label htmlFor="user_name">이름</label>
-        <Input
-          type="text"
+
+        <input
           id="user_name"
-          required
-          aria-required="true"
-          placeholder="이름을 입력해주세요"
-          onChange={(e) =>
-            setUserData({ ...userData, user_name: e.target.value })
-          }
+          type="user_name"
+          placeholder="*******"
+          {...register('user_name', {
+            required: '이름은 필수 입력입니다',
+            minLength: {
+              value: 2,
+              message: '이름은 최소 2글자 이상',
+            },
+          })}
+          aria-invalid={errors.user_name ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.user_name?.message === 'string'
+            ? errors.user_name.message
+            : ''}
+        </p>
       </div>
       <div>
         <label htmlFor="nickName">닉네임</label>
-        <Input
-          type="text"
+
+        <input
           id="nickName"
-          required
-          aria-required="true"
-          placeholder="닉네임을 입력해주세요"
-          onChange={(e) =>
-            setUserData({ ...userData, nickName: e.target.value })
-          }
+          type="nickName"
+          placeholder="*******"
+          {...register('nickName', {
+            required: '닉네임은 필수 입력입니다',
+            minLength: {
+              value: 2,
+              message: '두글자 이상으로 해주세요',
+            },
+          })}
+          aria-invalid={errors.nickName ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.nickName?.message === 'string'
+            ? errors.nickName.message
+            : ''}
+        </p>
       </div>
       <div>
         <label htmlFor="phone_number">휴대폰 번호</label>
-        <Input
-          type="tel"
+        <input
           id="phone_number"
-          required
-          aria-required="true"
-          placeholder="휴대폰 번호입력 ( - 제외 11자리 입력)"
-          onChange={(e) =>
-            setUserData({ ...userData, phone_number: e.target.value })
-          }
+          type="phone_number"
+          placeholder="*******"
+          {...register('phone_number', {
+            required: '전화번호는 필수 입력입니다',
+            pattern: {
+              value: /^(01[016789]{1})[0-9]{3,4}[0-9]{4}$/,
+              message: '"-"(하이픈)을 제외하고 입력해주세요.',
+            },
+          })}
+          aria-invalid={errors.phone_number ? 'true' : 'false'}
           className="border rounded-xl w-full h-[3rem] p-2 mt-2"
         />
+        <p className="text-red-600">
+          {typeof errors.phone_number?.message === 'string'
+            ? errors.phone_number.message
+            : ''}
+        </p>
       </div>
-      <div className="flex gap-3 justify-center items-end">
-        <label htmlFor="address" className="flex-1">
-          주소
-          <Input
-            type="text"
-            id="address"
-            value={userData.address}
-            required
-            aria-required="true"
-            placeholder="주소를 입력해주세요"
-            onChange={(e) =>
-              setUserData({ ...userData, address: e.target.value })
-            }
-            className="border rounded-xl w-full h-[3rem] p-2 mt-2"
-          />
-        </label>
-        <button
-          type="button"
-          className="border rounded-xl w-20 h-[3rem] hover:shadow-root"
-          onClick={handleAddress}
-        >
-          주소입력
-        </button>
+      <div>
+        <div className="flex gap-3 justify-center items-end">
+          <label htmlFor="address" className="flex-1">
+            주소
+            <input
+              id="address"
+              type="address"
+              placeholder="*******"
+              {...register('address', {
+                required: '주소 입력을 필수입니다.',
+                minLength: {
+                  value: 2,
+                  message: '최소 2글자이상 입력해주세요',
+                },
+              })}
+              aria-invalid={errors.address ? 'true' : 'false'}
+              className="border rounded-xl w-full h-[3rem] p-2 mt-2"
+            />
+          </label>
+          <button
+            type="button"
+            className="border rounded-xl w-20 h-[3rem] hover:shadow-root"
+            onClick={handleAddress}
+          >
+            주소입력
+          </button>
+        </div>
+        <p className="text-red-600">
+          {typeof errors.address?.message === 'string'
+            ? errors.address.message
+            : ''}
+        </p>
       </div>
+
       <div className="flex flex-col gap-3">
         <button
           type="submit"
@@ -214,6 +247,7 @@ function SignUpInput() {
         <button
           type="button"
           className="text-white text-xl rounded-xl w-full h-[3rem] bg-blue-primary hover:shadow-root"
+          onClick={() => window.history.back()}
         >
           취소
         </button>
