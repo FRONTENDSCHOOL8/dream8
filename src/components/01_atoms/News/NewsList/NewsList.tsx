c76f387ad6f0a095379123da65d5ec6d04173859
unknown <taewoo1212@gmail>
2024-04-16 16:28:16 +0900

    [Feat] : 소식(메인)페이지 반응형 구현  태욱
    [Feat] :    새로운 기능 추가
    [Modify] :    코드 변경
    [BugFix] :    버그 수정
    [Comment] :    필요한 주석 추가 및 변경
    [Docs] :    문서의 수정
    [Refactor] :    리팩토링
    [Design] :    CSS 등 사용자 UI 디자인 변경
    [Style] :    코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    [Test] :    테스트(테스트 코드 추가, 수정, 삭제, 비즈니스 로직에 변경이 없는 경우)
    [Chore] :    빌드 수정, 패키지 매니저 설정, 운영 코드 변경이 없는 경우(assets image, 패키지 매니저 등)
    [Init] :    프로젝트 초기 생성
    [Rename] :    파일 혹은 폴더명 수정하거나 옮기는 경우
    [Remove] :    파일을 삭제하는 작업만 수행하는 경우
    [Merge] :    Pull 과정 중 현재 commit과 자동 병합이 일어날 때
    [Build] :    새로운 라이브러리 혹은 패키지

--- src/components/01_atoms/News/NewsList/NewsList.tsx
+++ src/components/01_atoms/News/NewsList/NewsList.tsx
@@ -1,17 +1,26 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function NewsList({ newsList }) {
-
   return (
     <>
       {newsList.map((newsItem) => (
         <Link to={`/NewsDetails/${newsItem.id}`} key={newsItem.id}>
-          <div className="flex items-center justify-between border border-gray-200 rounded-[20px] p-5 m-5 hover:shadow-root transition-all">
+          <div className=" w-[500px] lg:w-[800px] flex items-center justify-between gap-10 border border-gray-200 rounded-[20px] p-5 my-5 mx-auto  hover:shadow-root transition-all">
             <ul className="flex flex-col gap-2">
-              <li><p className="text-xl font-bold">[{newsItem.type}] {newsItem.title}</p></li>
-              <li><span className="text-gray-500">{newsItem.content.substring(0, 50)}...</span></li>
+              <li>
+                <p className="text-xl font-bold">
+                  [{newsItem.type}] {newsItem.title}
+                </p>
+              </li>
+              <li>
+                <span className="text-gray-500">
+                  {newsItem.content.substring(0, 50)}...
+                </span>
+              </li>
             </ul>
-            <p className="text-xl">{new Date(newsItem.created).toLocaleDateString()}</p>
+            <p className="text-xl">
+              {new Date(newsItem.created).toLocaleDateString()}
+            </p>
           </div>
         </Link>
       ))}
